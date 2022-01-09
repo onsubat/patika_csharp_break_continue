80ab3aacef12411e2152e89bbcee7bd44056a3dc
onsubat <m.b.altintas@hotmail.com>
2022-01-10 00:59:53 +0300

    patika_csharp_break_continue

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("***ForLoop,Break,Continue***");
+
+
+for (int i = 1; i <= 8; i++){
+    if (i == 3){
+        Console.WriteLine("OMG ! " + i + " YOK ! O.O ! ");
+        continue;
+    }
+    else if (i == 8){
+        Console.WriteLine("YETEER");
+        break;
+    }
+    else {
+        Console.WriteLine(i + ". numara burada !");
+    }
+
+}
